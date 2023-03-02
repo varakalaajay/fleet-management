adc919c81ffefd7190bbaca042b57a1ceccc0e46
ajayvarakala <ajaykumar.varakala@criticalriver.com>
2023-03-02 14:32:35 +0530

    gps updated

--- src/components/LiveTracking.js
+++ src/components/LiveTracking.js
@@ -6,6 +6,7 @@ import newGeoPoints from "../../src/newpoints.json";
 import "leaflet/dist/leaflet.css";
 import "leaflet/dist/leaflet.js";
 import { Box, Toolbar } from "@mui/material";
+import axios from "axios";
 
 const dataStory = [
   {
@@ -33,20 +34,35 @@ const dataStory = [
 let cursor = 0;
 export default function LiveTracking() {
   const [currentTrack, setCurrentTrack] = useState({});
-
+  const token = localStorage.getItem("token");
+  const user = localStorage.getItem("user");
   useEffect(() => {
     setCurrentTrack(geopoints[cursor]);
 
     const interval = setInterval(() => {
-      if (cursor === geopoints.length - 1) {
+        const getDeviceLatLng = async () => {
+            const gpsres = await axios({
+              method: "post",
+              url: "http://174.138.121.17:8001/infinite/get_gps",
+              headers: {
+                "Content-Type": "application/octet-stream",
+                "x-token": token,
+                "x-user": user,
+              },
+              params: { device_id: "Device01", count: 1 },
+            });
+            setCurrentTrack({ lat: gpsres.data[0].lat, lng: gpsres.data[0].long });
+          };
+          getDeviceLatLng();
+      /* if (cursor === geopoints.length - 1) {
         cursor = 0;
         setCurrentTrack(geopoints[cursor]);
         return;
       }
 
       cursor += 1;
-      setCurrentTrack(geopoints[cursor]);
-    }, 1000);
+      setCurrentTrack(geopoints[cursor]); */
+    }, 5000);
     return () => {
       clearInterval(interval);
     };
